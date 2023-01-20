762a213c65e895e020be634efc4824db90a8cbc2
wonstruckk <wien0526@gmail.com>
2023-01-20 18:21:11 +0900

    layoutPrac

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
-            >
+            > 
               By{" "}
               <Image
                 src="/vercel.svg"
---
+++ src/commons/layout/navigation/index.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div`
+  height: 100px;
+  background-color: blue;
+`;
+
+export default function LayoutNavigation() {
+  return <Wrapper>여기는 네비게이션입니다.</Wrapper>;
+}
