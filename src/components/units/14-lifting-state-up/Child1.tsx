d3d3473ce616c2744207e0257b6c373f460c306a
wonstruckk <wien0526@gmail.com>
2023-01-22 23:54:55 +0900

    pagination

--- src/components/units/14-lifting-state-up/Child1.tsx
+++ src/components/units/14-lifting-state-up/Child1.tsx
@@ -4,7 +4,7 @@ export default function Child1() {
   return (
     <div>
       <div>자식1 카운트 : {props.count}</div>
-      <button onClick={onClickCountUp}>카운트 올리기.</button>
+      <button onClick={props.onClickCountUp}>카운트 올리기.</button>
     </div>
   );
 }
