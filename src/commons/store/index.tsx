4e9a9787fe400aeb2465615243feacc3306bda09
wonstruckk <wien0526@gmail.com>
2023-01-27 23:55:12 +0900

    accessToken try

--- src/commons/store/index.tsx
+++ src/commons/store/index.tsx
@@ -1,6 +1,12 @@
 import { atom } from "recoil";
 
+// redux store라고 생각하면 됨, 전역관리. atom
 export const isEditState = atom({
   key: "isEditState",
   default: false,
+});
+
+export const accessTokenState = atom({
+  key: "accessTokenState",
+  default: "",
 });
--- src/components/units/21-global-state/BoardWrite.container copy.tsx
+++ src/components/units/21-global-state/BoardWrite.container copy.tsx
@@ -1,7 +1,7 @@
 import { useRecoilState } from "recoil";
 import { isEditState } from "../../../commons/store";
 
-export default function BoardWriteUI(props) {
+export default function BoardWriteUI() {
   const [isEdit, setIsEdit] = useRecoilState(isEditState);
   return <div>{isEdit ? "use" : "recoil"}</div>;
 }
